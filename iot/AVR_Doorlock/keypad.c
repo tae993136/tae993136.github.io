7386a01587058252979d5f209e4db25b760bda58
tae993136 <62333360+tae993136@users.noreply.github.com>
2022-04-05 15:49:07 +0900

    Add files via upload

---
+++ iot/AVR_Doorlock/keypad.c
@@ -0,0 +1,47 @@
+﻿/*
+* keypad.c
+*
+* Created: 2022-04-04 오전 10:36:36
+*  Author: COMPUTER
+*/
+#define F_CPU 7372800
+
+#include "keypad.h"
+#include <util/delay.h>
+
+void init_keypad()
+{
+	KEYPADPIN |= ROW_OUT_COL_IN;		//DDRA = 0xf8;
+	KEYPADDATA &= ~(ROW_OUT_COL_IN);	//PORTA = 0X07;
+}
+
+unsigned char keyscan()
+{
+	ROWPIN = ROW0;
+	_delay_ms(10);
+	if(COLUMN&COL0)	return '1';
+	else if(COLUMN&COL1) return '2';
+	else if(COLUMN&COL2) return '3';
+	
+	ROWPIN = ROW1;
+	_delay_ms(10);
+	if(COLUMN&COL0)	return '4';
+	else if(COLUMN&COL1) return '5';
+	else if(COLUMN&COL2) return '6';
+	
+	ROWPIN = ROW2;
+	_delay_ms(10);
+	if(COLUMN&COL0)	return '7';
+	else if(COLUMN&COL1) return '8';
+	else if(COLUMN&COL2) return '9';
+	
+	ROWPIN = ROW3;
+	_delay_ms(10);
+	if(COLUMN&COL0)	return '*';
+	else if(COLUMN&COL1) return '0';
+	else if(COLUMN&COL2) return '#';
+	
+	return '\0';
+	
+}
+
---
+++ iot/AVR_Doorlock/keypad.h
@@ -0,0 +1,31 @@
+﻿/*
+ * keypad.h
+ *
+ * Created: 2022-04-04 오전 10:36:18
+ *  Author: COMPUTER
+ */ 
+
+#include <avr/io.h>
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#define KEYPADPIN	DDRA //0b 1111 1000
+#define KEYPADDATA	PORTA
+#define ROWPIN		PORTA		//출력
+#define ROW_OUT_COL_IN	0xF8
+#define COLUMN		PINA		//입력
+
+#define COL0	0x01
+#define COL1	0x02
+#define COL2	0x04
+#define ROW0	0x08
+#define ROW1	0x10
+#define ROW2	0x20
+#define ROW3	0x40
+
+void init_keypad();		//keypad 초기화
+unsigned char keyscan();//키스캔 함수
+
+
+
+#endif /* KEYPAD_H_ */
